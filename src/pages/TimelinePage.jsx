88bf74a5e719c33317048c0c542cbe8da88a4ca2
Matej Capkovic <matej.capkovic@vestberry.com>
2022-10-07 20:05:16 +0200

    add timeline

---
+++ src/pages/TimelinePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import timeline from '../components/timeline/timeline.svg'
+import Timeline from '../components/timeline/Timeline'
+import './timelinePage.css'
+
+function TimelinePage(){
+    return <div className='timeline-page'>
+        <Timeline />
+    </div>
+}
+
+export default TimelinePage
