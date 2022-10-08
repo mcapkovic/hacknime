ddc3355878620d6e1c24e99f75766deb65c496a3
Matej Capkovic <matej.capkovic@vestberry.com>
2022-10-08 11:32:02 +0200

    update story svg

--- src/App.js
+++ src/App.js
@@ -31,6 +31,10 @@ const router = createBrowserRouter([
         element: <StoryPage />,
       },
       {
+        path: ":tags",
+        element: <StoriesPage />,
+      },
+      {
         path: "faq",
         element: <FaqPage />,
       },
